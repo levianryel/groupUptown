ace7ee97fdf3d48038c1c5bb48ebc65aed3b46fb
levianryel <soundbyfume@gmail.com>
2026-02-18 08:11:02 +0800

    Created Repo and File

---
+++ groupUptownFile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace groupUptownFile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
